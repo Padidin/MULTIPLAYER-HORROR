ec00b14ca39081dd7a465a38a5c15b5375eba4ef
AchvatarCH <achvatarch@gmail.com>
2025-07-08 14:00:17 +0700

    Update CharSelect Scene

--- Assets/Vatar/Script/Manager/CharacterSelectManager.cs
+++ Assets/Vatar/Script/Manager/CharacterSelectManager.cs
@@ -25,25 +25,38 @@ public class CharacterSelectManager : MonoBehaviourPunCallbacks
         }
     }
 
-    public void SwtichCharacter1()
+    public void SwitchCharacter1()
     {
         if (char1 == false)
         {
-            SwitchChar1.Play();
-            SwitchChar2.Stop();
-            char1 = true;
-            char2 = false;
+            photonView.RPC("ClientChar1", RpcTarget.All);
+            
         }
     }
 
-    public void SwtichCharacter2()
+    public void SwitchCharacter2()
     {
         if (char2 == false)
         {
-            SwitchChar2.Play();
-            SwitchChar1.Stop();
-            char2 = true;
-            char1 = false;
+            photonView.RPC("ClientChar2", RpcTarget.All);
         }
+    }
+
+    [PunRPC]
+    void ClientChar1()
+    {
+        SwitchChar1.Play();
+        SwitchChar2.Stop();
+        char1 = true;
+        char2 = false;
+    }
+
+    [PunRPC]
+    void ClientChar2()
+    {
+        SwitchChar2.Play();
+        SwitchChar1.Stop();
+        char2 = true;
+        char1 = false;
     }
 }
