92fc64cf2c69eaf72c9b713f4310930cc4ea702e
AchvatarCH <achvatarch@gmail.com>
2025-11-15 01:15:37 +0700

    JANCOK GAK ISOK ISOK, NPC NENGOK PLAYER IKI YAOPO

--- Assets/Vatar/Script/HeadLookAtPlayer.cs
+++ Assets/Vatar/Script/HeadLookAtPlayer.cs
@@ -1,53 +1,50 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
-public class HeadLookSimple : MonoBehaviour
+public class HeadLookIK : MonoBehaviour
 {
-    public Transform headBone;     // mixamorig:Head
-    public Transform followTarget; // biasanya player HEAD / camera
-    public float smooth = 6f;
+    public float distance = 3f;
+    public Animator anim;
+    public Transform headBone;
+    public Transform lookTarget;
+    private bool canLook = false;
 
-    [Header("Clamp")]
-    public float maxYaw = 60f;     // kiri-kanan
-    public float maxPitch = 30f;   // atas-bawah
-
-    private Quaternion defaultRot;
+    public PlayableDirector director;
 
     void Start()
     {
-        defaultRot = headBone.localRotation; // simpan rotasi awal
+        director.stopped += OnTimelineFinished;
+    }
+
+    void OnTimelineFinished(PlayableDirector d)
+    {
+        Debug.Log("Timeline selesai BRO!");
+        canLook = true;
     }
 
     void LateUpdate()
     {
-        if (headBone == null || followTarget == null) return;
+        if (!canLook) return;
 
-        // arah target dalam LOCAL SPACE head parent
-        Vector3 localDir = headBone.parent.InverseTransformPoint(followTarget.position);
+        float dist = Vector3.Distance(transform.position, lookTarget.position);
 
-        // dapatkan rotasi yang diinginkan
-        Quaternion lookRot = Quaternion.LookRotation(localDir);
+        // Jika jarak masuk radius override kepala
+        if (dist < distance)
+        {
+            LookAtPlayer();
+        }
+    }
 
-        // convert ke Euler biar bisa di-clamp
-        Vector3 euler = lookRot.eulerAngles;
+    void LookAtPlayer()
+    {
+        Vector3 dir = lookTarget.position - headBone.position;
+        Quaternion targetRot = Quaternion.LookRotation(dir);
 
-        // Benerin nilai jadi -180 sampai 180
-        if (euler.y > 180) euler.y -= 360;
-        if (euler.x > 180) euler.x -= 360;
-
-        // clamp kanan-kiri (yaw)
-        euler.y = Mathf.Clamp(euler.y, -maxYaw, maxYaw);
-
-        // clamp atas-bawah (pitch)
-        euler.x = Mathf.Clamp(euler.x, -maxPitch, maxPitch);
-
-        // gabung lagi
-        Quaternion clampedRot = Quaternion.Euler(euler);
-
-        // apply smooth
-        headBone.localRotation = Quaternion.Slerp(
-            headBone.localRotation,
-            defaultRot * clampedRot,
-            Time.deltaTime * smooth
+        // Smooth rotasi biar gak robot
+        headBone.rotation = Quaternion.Slerp(
+            headBone.rotation,
+            targetRot,
+            Time.deltaTime * 5f
         );
     }
 }
